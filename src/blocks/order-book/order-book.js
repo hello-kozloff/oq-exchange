a97589d2eb75cb158d093a6153334efb65135668
hello.kozloff <hello.kozloff@gmail.com>
2020-03-10 15:37:53 +0300

    create order book block

---
+++ src/blocks/order-book/order-book.js
@@ -0,0 +1,35 @@
+$(document).ready(() => {
+
+  $(".order-book__view_buy").on("click", function (e) {
+    e.preventDefault();
+
+    $(".order-book__view").removeClass("order-book__view_selected");
+    $(this).addClass("order-book__view_selected");
+
+    $(".order-book__area_sell").hide();
+    $(".order-book__area_buy").show();
+  });
+
+  $(".order-book__view_split").on("click", function (e) {
+    e.preventDefault();
+
+    $(".order-book__view").removeClass("order-book__view_selected");
+    $(this).addClass("order-book__view_selected");
+
+    $(".order-book__area_buy").show();
+    $(".order-book__area_sell").show();
+  });
+
+  $(".order-book__view_sell").on("click", function (e) {
+    e.preventDefault();
+
+    $(".order-book__view").removeClass("order-book__view_selected");
+    $(this).addClass("order-book__view_selected");
+
+    $(".order-book__area_buy").hide();
+    $(".order-book__area_sell").show();
+  });
+
+  $(".order-book__select select").niceSelect();
+
+});
