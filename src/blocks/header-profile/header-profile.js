6fcbff1387423ba20468af1f5dbc3d93b38d91a7
hello.kozloff <hello.kozloff@gmail.com>
2020-03-14 14:35:37 +0300

    add toggle function inn header profile block

--- src/blocks/header-profile/header-profile.js
+++ src/blocks/header-profile/header-profile.js
@@ -1,17 +1,23 @@
 $(document).ready(() => {
 
   $(".header-profile__button").on("click", function (e) {
-    e.preventDefault();
-
-    $(".header-profile").toggleClass("header-profile_toggle");
+    toggleHeaderProfile();
   });
 
   $(document).mouseup(function (e){
     const div = $(".header-profile");
 
     if (!div.is(e.target) && div.has(e.target).length === 0 && $(".header-profile").hasClass("header-profile_toggle")) {
-      $(".header-profile").toggleClass("header-profile_toggle");
+      toggleHeaderProfile();
     }
   });
+
+  /**
+   * The function toggle header profile.
+   */
+  function toggleHeaderProfile() {
+    $(".header-profile").toggleClass("header-profile_toggle");
+    $(".header-profile__icon").toggleClass("header-profile__icon_toggle");
+  }
 
 });
